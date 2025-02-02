88bcf125386cdc5b27c01809c8233562a84f6ed5
bh2212 <kbhuvaneshwari2212@gmail.com>
2025-02-02 19:35:20 +0530

    Update ChatClient.java

--- ChatClient.java
+++ ChatClient.java
@@ -1,1 +1,46 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
+public class ChatClient {
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 12345;
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            
+            Thread readThread = new Thread(new ReadHandler(in));
+            readThread.start();
+
+            Scanner scanner = new Scanner(System.in);
+            String message;
+            while ((message = scanner.nextLine()) != null) {
+                out.println(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ReadHandler implements Runnable {
+    private BufferedReader in;
+
+    public ReadHandler(BufferedReader in) {
+        this.in = in;
+    }
+
+    @Override
+    public void run() {
+        String message;
+        try {
+            while ((message = in.readLine()) != null) {
+                System.out.println(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
